231d26f918bbfd2419331a26b723be72252f7c64
Park June Chul <pjc0247@naver.com>
2018-03-27 18:16:47 +0900

    update

---
+++ lookatme/Obfuscator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+using Mono.Cecil.Mdb;
+using Mono.CompilerServices.SymbolWriter;
+
+namespace lookatme
+{
+    public class Obfuscator
+    {
+        public static void Run(string inputPath, string outputPath)
+        {
+            try
+            {
+                var module = ModuleDefinition.ReadModule(inputPath);
+
+                foreach (var rf in module.AssemblyReferences)
+                    Console.WriteLine(rf);
+
+                Injector.Inject(module, typeof(lookatme.corelib.AssemblyTag).Assembly);
+
+                foreach (var type in Assembly.GetExecutingAssembly().GetTypes())
+                {
+                    if (type.BaseType != (typeof(PatternBase))) continue;
+
+                    var inst = Activator.CreateInstance(type);
+                    typeof(PatternBase).GetMethod(nameof(PatternBase.Execute)).Invoke(inst, new object[] { module });
+                }
+
+                module.Write(outputPath);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
--- lookatme/Program.cs
+++ lookatme/Program.cs
@@ -5,40 +5,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using Mono.Cecil;
-using Mono.Cecil.Cil;
-using Mono.Cecil.Mdb;
-using Mono.CompilerServices.SymbolWriter;
-
 namespace lookatme
 {
     class Program
     {
         static void Main(string[] args)
         {
-            try
-            {
-                var module = ModuleDefinition.ReadModule("..\\..\\..\\sampleapp\\bin\\Debug\\sampleapp.exe");
-
-                foreach (var rf in module.AssemblyReferences)
-                    Console.WriteLine(rf);
-
-                Injector.Inject(module, typeof(lookatme.corelib.AssemblyTag).Assembly);
-
-                foreach (var type in Assembly.GetExecutingAssembly().GetTypes())
-                {
-                    if (type.BaseType != (typeof(PatternBase))) continue;
-
-                    var inst = Activator.CreateInstance(type);
-                    typeof(PatternBase).GetMethod(nameof(PatternBase.Execute)).Invoke(inst, new object[] { module });
-                }
-
-                module.Write("output.exe");
-            }
-            catch(Exception e)
-            {
-                Console.WriteLine(e);
-            }
+            Obfuscator.Run(
+                "..\\..\\..\\sampleapp\\bin\\Debug\\sampleapp.exe",
+                "output.exe");
         }
     }
 }
