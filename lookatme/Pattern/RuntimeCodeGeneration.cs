c97e408ade7d8d164acd6f58cb67d35f3ed2bb05
Park June Chul <pjc0247@naver.com>
2018-03-28 13:52:59 +0900

    update

--- lookatme/Pattern/RuntimeCodeGeneration.cs
+++ lookatme/Pattern/RuntimeCodeGeneration.cs
@@ -41,70 +41,76 @@ namespace lookatme
             public StoredInstruction[] instructions;
         }
 
+        private MethodData PackMethod(MethodDefinition method)
+        {
+            var sinst = new List<StoredInstruction>();
+            foreach (var inst in method.Body.Instructions)
+            {
+                if (inst.Operand is Instruction)
+                    continue;
+
+                var _op = inst.Operand;
+                if (_op is TypeReference)
+                {
+                    var op = (TypeReference)_op;
+                    _op = new StoredTypeReference()
+                    {
+                        name = op.Name,
+                        ns = op.Namespace
+                    };
+                }
+                else if (_op is FieldReference)
+                {
+                    var op = (FieldReference)_op;
+                    _op = new StoredFieldReference()
+                    {
+                        name = op.Name
+                    };
+                }
+                else if (_op is MethodReference)
+                {
+                    var op = (MethodReference)_op;
+                    _op = new StoredMethodReference()
+                    {
+                        name = op.Name
+                    };
+                }
+                else if (_op is VariableReference)
+                {
+                    var op = (VariableReference)_op;
+                    _op = new StoredVariableReference()
+                    {
+                    };
+                }
+
+                sinst.Add(new StoredInstruction()
+                {
+                    opcode = inst.OpCode,
+                    operand = _op
+                });
+
+                if (inst.Operand != null)
+                    Console.WriteLine(inst.Operand.GetType());
+            }
+
+            var methodData = new MethodData()
+            {
+                instructions = sinst.ToArray()
+            };
+
+            Console.WriteLine(JsonConvert.SerializeObject(methodData, new JsonSerializerSettings()
+            {
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+            }));
+
+            return methodData;
+        }
+
         public override void EachType(TypeDefinition type)
         {
             foreach (var method in type.Methods)
             {
-                var sinst = new List<StoredInstruction>();
-                foreach (var inst in method.Body.Instructions)
-                {
-                    if (inst.Operand is Instruction)
-                        continue;
-
-                    var _op = inst.Operand;
-                    if (_op is TypeReference)
-                    {
-                        var op = (TypeReference)_op;
-                        _op = new StoredTypeReference()
-                        {
-                            name = op.Name,
-                            ns = op.Namespace
-                        };
-                    }
-                    else if (_op is FieldReference)
-                    {
-                        var op = (FieldReference)_op;
-                        _op = new StoredFieldReference()
-                        {
-                            name = op.Name
-                        };
-                    }
-                    else if (_op is MethodReference)
-                    {
-                        var op = (MethodReference)_op;
-                        _op = new StoredMethodReference()
-                        {
-                            name = op.Name
-                        };
-                    }
-                    else if (_op is VariableReference)
-                    {
-                        var op = (VariableReference)_op;
-                        _op = new StoredVariableReference()
-                        {
-                        };
-                    }
-
-                    sinst.Add(new StoredInstruction()
-                    {
-                        opcode = inst.OpCode,
-                        operand = _op
-                    });
-
-                    if (inst.Operand != null)
-                    Console.WriteLine(inst.Operand.GetType());
-                }
-
-                var methodData = new MethodData()
-                {
-                    instructions = sinst.ToArray()
-                };
-
-                
-                Console.WriteLine(JsonConvert.SerializeObject(methodData, new JsonSerializerSettings()
-                {
-                    ReferenceLoopHandling = ReferenceLoopHandling.Ignore
-                }));
+                PackMethod(method);
             }
         }
     }
